55d4fc98a2ee5f119ae33d66fc64cec4ec3e3f3d
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-01-24 17:24:05 +0900

    chore: add rules in eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,57 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	plugins: ['svelte3', '@typescript-eslint'],
+  rules: {
+    'arrow-parens': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'consistent-return': ['error', { treatUndefinedAsUnspecified: false }],
+    'eol-last': ['error', 'always'],
+    'func-style': ['error', 'expression'],
+    'indent': ['error', 2],
+    'no-console': 'error',
+    'no-else-return': ['error', { allowElseIf: false }],
+    'no-param-reassign': ['error', { props: true }],
+    'no-return-assign': ['error', 'except-parens'],
+    'no-tabs': ['error', { allowIndentationTabs: false }],
+    'no-useless-return': 'error',
+    'quote-props': ['error', 'consistent-as-needed'],
+    // rules that extend base eslint rules
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
+    'quotes': 'off',
+    '@typescript-eslint/quotes': ['error', 'single'],
+    'semi': 'off',
+    '@typescript-eslint/semi': ['error', 'always'],
+    // rules that are typescript only
+    '@typescript-eslint/consistent-type-assertions': [
+      'error',
+      {
+        assertionStyle: 'as',
+        objectLiteralTypeAssertions: 'never',
+      }],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'semi',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: true,
+        },
+        multilineDetection: 'brackets',
+      }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-inferrable-types': [
+      'error',
+      {
+        ignoreParameters: true,
+        ignoreProperties: true,
+      },
+    ],
+  },
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
