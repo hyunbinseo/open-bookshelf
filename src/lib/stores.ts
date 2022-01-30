873c2cd3dec9e372d81ba39ed798cc9baacd41c2
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-01-30 12:47:56 +0900

    feat: convert filteredStories to store

---
+++ src/lib/stores.ts
@@ -0,0 +1,36 @@
+import { writable, derived } from 'svelte/store';
+
+import stories from '$lib/stories/data';
+
+import type { SortOption, Language, Level, Topic } from '$lib/stories/types';
+
+export const isLoaded = writable<boolean>(false);
+
+export const selSort = writable<SortOption>('number');
+export const selLanguage = writable<Language>('ko-kr');
+export const selLevel = writable<Level | undefined>();
+export const selTopic = writable<Topic | undefined>();
+
+export const reqStories = derived(
+  [isLoaded, selSort, selLanguage, selLevel, selTopic],
+  ([$isLoaded, $selSort, $selLanguage, $selLevel, $selTopic]) => {
+    if (!$isLoaded) return [];
+
+    const filtered = stories
+      .filter(({ language }) => language === $selLanguage)
+      .filter(({ level }) => ($selLevel ? level === $selLevel : true))
+      .filter(({ topics }) => ($selTopic ? topics.has($selTopic) : true));
+
+    // Data is currently sorted by story number
+    if ($selSort === 'number') return filtered;
+
+    if ($selSort === 'abc') return filtered
+      .sort(({ title: titleA }, { title: titleB }) => {
+        if (titleA < titleB) return -1;
+        if (titleA > titleB) return 1;
+        return 0;
+      });
+
+    return filtered;
+  },
+);
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,50 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	import stories from '$lib/stories/data';
-
-	import { languageEnum } from '$lib/stories/types';
-	import { languages, levels, topics } from '$lib/stories/types';
+	import {
+		sortOptions,
+		sortOptionEnum,
+		languages,
+		languageEnum,
+		levels,
+		topics
+	} from '$lib/stories/types';
 	import { isLanguage, isLevel, isTopic } from '$lib/stories/types';
 
-	import type { Language, Level, Topic } from '$lib/stories/types';
-
-	// Required value(s)
-	let selLanguage: Language;
-
-	// Optional value(s)
-	let selLevel: Level;
-	let selTopic: Topic;
-
-	$: filteredStories = !selLanguage
-		? [] // Empty before mount
-		: stories
-				.filter(({ language }) => language === selLanguage)
-				.filter(({ level }) => (selLevel ? level === selLevel : true))
-				.filter(({ topics }) => (selTopic ? topics.has(selTopic) : true));
+	import { isLoaded, selLanguage, selLevel, selTopic, reqStories, selSort } from '$lib/stores';
 
 	onMount(() => {
 		const { searchParams } = new URL(document.URL);
 
-		// Default language value is set on mount
 		const reqLanguage = searchParams.get('language');
-		selLanguage = isLanguage(reqLanguage) ? reqLanguage : 'ko-kr';
+		if (isLanguage(reqLanguage)) selLanguage.set(reqLanguage);
 
 		const reqTopic = searchParams.get('topic');
-		if (isTopic(reqTopic)) selTopic = reqTopic;
+		if (isTopic(reqTopic)) selTopic.set(reqTopic);
 
 		const reqLevel = Number(searchParams.get('level'));
-		if (isLevel(reqLevel)) selLevel = reqLevel;
+		if (isLevel(reqLevel)) selLevel.set(reqLevel);
 
 		history.replaceState(null, '', '/');
+
+		isLoaded.set(true);
 	});
 </script>
+
+<h3>정렬</h3>
+
+{#each sortOptions as sortOption}
+	<label>
+		<input type="radio" bind:group={$selSort} name="sortOption" value={sortOption} />
+		{sortOptionEnum[sortOption]}
+	</label>
+{/each}
 
 <h3>언어</h3>
 
 {#each languages as language}
 	<label>
-		<input type="radio" bind:group={selLanguage} name="language" value={language} />
+		<input type="radio" bind:group={$selLanguage} name="language" value={language} />
 		{languageEnum[language]}
 	</label>
 {/each}
@@ -53,13 +53,13 @@
 
 <button
 	on:click={() => {
-		selTopic = undefined;
+		selTopic.set(undefined);
 	}}>초기화</button
 >
 
 {#each topics as topic}
 	<label>
-		<input type="radio" bind:group={selTopic} name="topic" value={topic} />
+		<input type="radio" bind:group={$selTopic} name="topic" value={topic} />
 		{topic}
 	</label>
 {/each}
@@ -68,36 +68,34 @@
 
 <button
 	on:click={() => {
-		selLevel = undefined;
+		selLevel.set(undefined);
 	}}>초기화</button
 >
 
 {#each levels as level}
 	<label>
-		<input type="radio" bind:group={selLevel} name="level" value={level} />
+		<input type="radio" bind:group={$selLevel} name="level" value={level} />
 		{level}
 	</label>
 {/each}
 
 <h3>이야기</h3>
 
-{#if filteredStories.length === 0}
-	{#if !selLanguage}
-		불러오는 중입니다.
-	{:else}
-		검색 결과가 없습니다.
-		<button
-			on:click={() => {
-				selTopic = undefined;
-				selLevel = undefined;
-			}}
-		>
-			필터 초기화
-		</button>
-	{/if}
+{#if !$isLoaded}
+	불러오는 중입니다.
+{:else if !$reqStories.length}
+	검색 결과가 없습니다.
+	<button
+		on:click={() => {
+			selTopic.set(undefined);
+			selLevel.set(undefined);
+		}}
+	>
+		필터 초기화
+	</button>
 {:else}
 	<ul>
-		{#each filteredStories as story}
+		{#each $reqStories as story}
 			<li>
 				<a
 					href={`http://xn--hu1b40go5ck8x.com/e-book/ecatalog5.php?Dir=${story.id}&catimage=1`}
