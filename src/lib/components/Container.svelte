74945409be01bbf2ce448657d9424201c0badf88
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-01-31 18:35:59 +0900

    feat: create loading, filter reset components

--- src/lib/components/Container.svelte
+++ src/lib/components/Container.svelte
@@ -1,3 +1,11 @@
-<div class="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8 2xl:max-w-screen-2xl">
+<script lang="ts">
+	export let isFullHeight = false;
+</script>
+
+<div
+	class={`mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8 2xl:max-w-screen-2xl ${
+		isFullHeight ? 'h-full' : ''
+	}`}
+>
 	<slot />
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -35,26 +35,34 @@
 
 <svelte:window bind:scrollY />
 
-<div class="flex-1 bg-gray-50">
+<div class="flex flex-1 flex-col bg-gray-50">
 	<Sidebar />
 	<Header heading="두루책방" paragraph="찾아줘서 고마워요! 읽고 싶은 책을 찾아볼까요?" />
 	<Filters />
-	<Container>
-		<section aria-labelledby="stories-heading" class="mt-2 pb-16">
-			<h2 id="stories-heading" class="sr-only">이야기 목록</h2>
-			{#if !$isLoaded}
-				<p>불러오는 중입니다.</p>
-			{:else if !$reqStories.length}
-				<p>검색 결과가 없습니다.</p>
+	<section aria-labelledby="stories-heading" class="mt-2 flex-1 pb-16">
+		<h2 id="stories-heading" class="sr-only">이야기 목록</h2>
+		<Container isFullHeight={true}>
+			{#if !$reqStories.length}
 				<button
+					type="button"
+					class="relative block h-full w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+					disabled={!$isLoaded}
 					on:click={() => {
 						collapseAllDropdown();
 						selTopic.set(undefined);
 						selLevel.set(undefined);
-						scrollY = 0;
 					}}
 				>
-					검색 조건 초기화
+					{#if !$isLoaded}
+						<h3 class="text-lg font-medium leading-6 text-gray-900">책을 찾고 있습니다.</h3>
+						<p class="mt-2 text-xs text-gray-500">불러와지지 않으면 꼬리말의 링크를 사용합니다.</p>
+					{:else}
+						<h3 class="text-lg font-medium leading-6 text-gray-900">책을 찾을 수 없습니다.</h3>
+						<p class="mt-2 text-sm text-indigo-500">
+							상세 검색 조건 초기화
+							<span aria-hidden="true">&rarr;</span>
+						</p>
+					{/if}
 				</button>
 			{:else}
 				<div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
@@ -64,8 +72,8 @@
 				</div>
 				<p class="pt-16 text-center lg:hidden">목록의 끝입니다.</p>
 			{/if}
-		</section>
-	</Container>
+		</Container>
+	</section>
 </div>
 <Footer>
 	<a
