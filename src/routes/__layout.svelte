c57f2b67d9d7766f1580a75c947fe30401b76460
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-02-03 21:42:40 +0900

    feat: update page description

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,7 +5,7 @@
 
 	import stories from '$lib/stories/data';
 
-	const description = `다양한 주제와 수준의 이야기 ${stories.length}개를 따라 읽고 들으세요. 느린 학습자를 위한 쉬운 책, 다문화 가정 어린이를 위한 다국어 책도 있습니다.`;
+	const description = `다양한 주제와 수준의 이야기 ${stories.length}개를 듣고 따라 읽으세요. 누구에게나 쉬운 책부터 여러 언어로 쓰인 다국어 책까지 모두 준비되어 있어요.`;
 	const ogImageURl = `${VITE_CDN_URL}/image/open-graph.jpg`;
 </script>
 
