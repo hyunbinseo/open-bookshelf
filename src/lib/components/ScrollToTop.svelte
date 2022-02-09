3a6c3c47aae40f2cd166a6aea1f92a3057bab396
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-02-09 19:53:00 +0900

    fix: remove disable in scroll-to-top button
    disable is buggy when scrolling fast in touch

--- src/lib/components/ScrollToTop.svelte
+++ src/lib/components/ScrollToTop.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import { fly } from 'svelte/transition';
 
-	let disabled = false;
-
 	let scrollY: number;
 </script>
 
@@ -10,12 +8,10 @@
 
 <div transition:fly={{ y: 50 }} class="sticky bottom-10 mx-auto">
 	<button
-		{disabled}
 		type="button"
-		class="inline-flex items-center rounded-full border border-transparent bg-gray-600 p-3 text-white transition duration-300 hover:bg-gray-700 disabled:bg-opacity-50"
+		class="inline-flex items-center rounded-full border border-transparent bg-gray-600 p-3 text-white transition duration-300 hover:bg-gray-700"
 		style="box-shadow: rgb(0 0 0 / 20%) 0px 3px 3px -2px, rgb(0 0 0 / 14%) 0px 3px 4px 0px, rgb(0 0 0 / 12%) 0px 1px 8px 0px;"
-		on:click|once={() => {
-			disabled = true;
+		on:click={() => {
 			scrollY = 0;
 		}}
 	>
